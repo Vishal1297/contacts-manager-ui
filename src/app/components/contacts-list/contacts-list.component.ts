da720d7c65ae486588bd994a3f22337497aaf9df
Vishal Yadav <vishalyadav1746@gmail.com>
2022-10-22 21:44:39 +0530

    fix: deployment & add todo

--- src/app/components/contacts-list/contacts-list.component.ts
+++ src/app/components/contacts-list/contacts-list.component.ts
@@ -21,6 +21,8 @@ export class ContactsListComponent implements OnInit {
     this.fetchAllContacts();
   }
 
+  // TODO: Success/Failure handling on api calls
+
   fetchAllContacts(): void {
     this.contactsService.getAll()
       .pipe(pluck('data'))
