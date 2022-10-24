03d8cef757b767cc85a5916ff6d89f5bb44b5154
Vishal Yadav <Vishalyadav1746@gmail.com>
2022-10-24 11:39:12 +0530

    Revert "feat(deployment): add deployment files - Dockerfile,env,nginx."
    This reverts commit 67b691c1fe7aa6169d221de2ce61ed5de3735b93.

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { BASE_URL } from 'src/environments/environment';
 import { Contact } from '../models/models';
 
+const baseUrl = 'http://localhost:8080/contact/v1'
 
 @Injectable({
   providedIn: 'root'
@@ -13,27 +13,27 @@ export class ContactsService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`${BASE_URL}/contacts`);
+    return this.http.get<Contact[]>(`${baseUrl}/contacts`);
   }
 
   get(id: string): Observable<Contact> {
-    return this.http.get(`${BASE_URL}/contact/${id}`);
+    return this.http.get(`${baseUrl}/contact/${id}`);
   }
 
   createOrUpdate(data: Contact): Observable<Contact> {
-    return this.http.post(`${BASE_URL}/contact`, data);
+    return this.http.post(`${baseUrl}/contact`, data);
   }
 
   delete(id: string): Observable<Contact> {
-    return this.http.delete(`${BASE_URL}/contact/${id}`);
+    return this.http.delete(`${baseUrl}/contact/${id}`);
   }
 
   deleteAll(): Observable<Contact> {
-    return this.http.delete(`${BASE_URL}/contacts/all`);
+    return this.http.delete(`${baseUrl}/contacts/all`);
   }
 
   searchByFullName(name: string): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`${BASE_URL}/contacts/name/${name}`);
+    return this.http.get<Contact[]>(`${baseUrl}/contacts/name/${name}`);
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true
 };
-
-export const BASE_URL = 'http://localhost:8080/contact/v1'
