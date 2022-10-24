2685d1df210b1f1f16d3caf6446664b5682acc9d
Vishal Yadav <Vishalyadav1746@gmail.com>
2022-10-24 11:39:12 +0530

    Revert "fix: deployment & add todo"
    This reverts commit da720d7c65ae486588bd994a3f22337497aaf9df.

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { BASE_URL } from 'src/environments/environment';
 import { Contact } from '../models/models';
 
 
@@ -13,27 +13,27 @@ export class ContactsService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`${environment.BASE_URL}/contacts`);
+    return this.http.get<Contact[]>(`${BASE_URL}/contacts`);
   }
 
   get(id: string): Observable<Contact> {
-    return this.http.get(`${environment.BASE_URL}/contact/${id}`);
+    return this.http.get(`${BASE_URL}/contact/${id}`);
   }
 
   createOrUpdate(data: Contact): Observable<Contact> {
-    return this.http.post(`${environment.BASE_URL}/contact`, data);
+    return this.http.post(`${BASE_URL}/contact`, data);
   }
 
   delete(id: string): Observable<Contact> {
-    return this.http.delete(`${environment.BASE_URL}/contact/${id}`);
+    return this.http.delete(`${BASE_URL}/contact/${id}`);
   }
 
   deleteAll(): Observable<Contact> {
-    return this.http.delete(`${environment.BASE_URL}/contacts/all`);
+    return this.http.delete(`${BASE_URL}/contacts/all`);
   }
 
   searchByFullName(name: string): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`${environment.BASE_URL}/contacts/name/${name}`);
+    return this.http.get<Contact[]>(`${BASE_URL}/contacts/name/${name}`);
   }
 
 }
